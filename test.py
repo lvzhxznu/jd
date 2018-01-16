56bc8c0c2510fe88ef16d52d9d3f8644bf0a4197
lvzhxznu <lvzhxznu@163.com>
2018-01-16 10:37:54 +0800

    test

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-print('hello world!')
-print(33)
+import  requests
+
+from pyquery import PyQuery as pq
+
+from lxml import  etree
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'}
+
+
+s = requests.session()
+
+s.get("https://www.jd.com", headers = headers, timeout =1)
+
+r = s.get("https://www.jd.com", headers = headers, timeout = 1)
+
+
+#print(r.text)
+
+print(r.request.headers)
+
+print(r.headers)
+
+print(r.cookies)
